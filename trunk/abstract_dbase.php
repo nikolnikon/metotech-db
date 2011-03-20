114d1980209cecabc9e11a4d4cf1c16bb74240bb
nikolnikon <nikolnikon@b4632097-a9eb-b918-c771-14d8c8402607>
2011-03-20 14:56:02 +0000

    Создаю хранилище

---
+++ trunk/abstract_dbase.php
@@ -0,0 +1,60 @@
+<?php
+abstract class AbstractDBase
+{
+    private $m_dbAddress; /* Address of SQL server */
+    private $m_dbUser; /* Account name (username) */
+    private $m_dbPwd;     /* Password */
+    private $m_dbName;    /* Initial database */
+ 
+    /* Base constructor */
+    public function __construct($address, $account, $pwd, $name) {
+        $this->m_dbAddress = $address;
+        $this->m_dbUser = $account;
+        $this->m_dbPwd = $pwd;
+        $this->m_dbName = $name;
+ 
+        /* Call the connect method in the child class */
+        $this->connect($this->m_dbAddress, $this->m_dbUser,
+            $this->m_dbPwd, $this->m_dbName);
+    }
+ 
+    /* Let the child disconnect when the object is destroyed */
+    public function __destruct() {
+        $this->disconnect();
+    }
+    
+    abstract public function select($query);
+    //abstract public function insert();
+    //abstract public function update();
+    //abstract public function delete();
+    
+    //abstract function fetchRow();
+    //abstract function fetchAll();
+    //abstract function getNumRows();
+    //abstract function freeResult();
+    //abstract function getError();
+ 
+    /* We are about to be serialized, disconnect
+     * and return data needed for serialization
+     */
+    public function __sleep() {
+        $this->disconnect();
+        return array('m_dbAddress', 'm_dbUser',
+            'm_dbPwd', 'm_dbName');
+    }
+ 
+    /* We have been unserialized, re-connect */
+    public function __wakeup() {
+        $this->connect($this->m_dbAddress, $this->m_dbUser,
+            $this->m_dbPwd, $this->m_dbName);
+    }
+ 
+    /*
+     * Abstract methods that needs to be implemented by child classes
+     */
+    abstract protected function connect($address, $account,
+        $pwd, $name);
+    abstract protected function disconnect();
+    
+}
+?>
---
+++ trunk/mysql_dbase.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once('db_iface.inc');
+
+class MySQLDBase
+{
+	private $m_dbConn;
+    private $m_Result;
+ 
+    /* 
+     * Connect function, also selects correct database
+     * Returns 1 upon success, otherwise 0
+     */
+    protected function connect($address, $user, $pwd, $name) {
+        $this->m_dbConn = @mysql_connect($address, $user, $pwd);
+ 
+        if (! $this->m_dbConn) 
+        	throw new Exception("Не удалось соединиться с базой данных", E_USER_ERROR);
+        
+        if (! mysql_select_db($name, $this->m_dbConn))
+        	throw new Exception("Не удалось выбрать базу данных", E_USER_ERROR);
+    }	
+ 
+    /*
+     * Disconnect function
+     */
+    protected function disconnect() {
+    	@mysql_close($this->m_dbConn);
+    }
+    
+    /*Функция выполняет запрос SELECT и возвращает выбору в виде двумерного ассоциативного массива*/
+    public function select($query) {
+    	$this->m_Result = mysql_query($query, $this->m_dbConn);
+    	if (! $this->m_Result) {
+    		throw new Exception("Ошибка при выполнении запроса.\n".mysql_error($this->m_dbConn));
+    	}
+    	$arReturn = array();
+    	while ($row = mysql_fetch_assoc()) {
+    		$arReturn[] = $row;
+    	}
+    	
+    	return $arReturn;
+    }
+ 
+    /*
+     * Does a mysql-query, returns 1 upon sucess otherwise 0
+     */
+    /*function query($query) {
+        $this->m_Result = mysql_query($query, $this->m_dbConn);
+ 
+        if ($this->m_Result != 0) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+ 
+    /*
+     * Fetches an array row
+     */
+    /*function fetchRow() {
+        return mysql_fetch_array($this->m_Result);
+    }
+ 
+    function fetchAll() {
+        while ($row = mysql_fetch_array($this->m_Result)) {
+            $a_rs[] = $row;
+        }
+        mysql_free_result($this->m_Result);
+        return $a_rs;
+    }
+    /*
+     * Get number of rows
+     */
+    /*function getNumRows() {
+        return mysql_num_rows($this->m_Result);
+    }
+ 
+    /*
+     * Free resources allocated by a query 
+     */
+    /*function freeResult() {
+        mysql_free_result($this->m_Result);
+    }
+ 
+    /*
+     * Get errorstring
+     */
+    /*function getError() {
+        return mysql_error($this->m_dbConn);
+    }*/
+}
+?>
