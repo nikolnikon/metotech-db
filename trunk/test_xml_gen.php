dbffd365ddf8d2fc09d0863713fbf7974a204bc8
nikolnikon <nikolnikon@b4632097-a9eb-b918-c771-14d8c8402607>
2011-08-04 13:52:49 +0000

    1. Пробую работу с SimpleXML на php. Добавил временные файлы для тестирования.

---
+++ trunk/test_xml_gen.php
@@ -0,0 +1,12 @@
+<?php
+	require_once 'class.price_list.php';
+	
+	try {
+		$db = new MySQLDBase('127.0.0.1', 'root', '', 'metalls');
+	}
+	catch(Exception $e) {
+		echo "Îøèáêà\n".$e->getMessage();
+	}
+	$price = new PriceList($db, "nikel-price", $_GET);
+	$price->getXML();
+?>
